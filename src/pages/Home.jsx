29ff601960bc1bb82d21fb3ed1a3fa0667a209f5
Inayat Ullah Khan <khan99.inayat@gmail.com>
2024-12-26 16:08:05 +0530

    some changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
-
-import Advertise from "@/components/shared/Advertise";
+import Advertise from "../components/shared/Advertise"
+import PostCard from "../components/shared/PostCard"
 import { Button } from "../components/ui/button"
 import { ArrowRight } from "lucide-react"
 import React, { useEffect, useState } from "react"
@@ -8,6 +8,8 @@ const apiUrl=import.meta.env.VITE_BACKENT_API_URL;
 
 const Home = () => {
   const [posts, setPosts] = useState([])
+
+  // console.log(posts)
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -78,11 +80,28 @@ const Home = () => {
       </section>
 
       <div className="p-3 bg-white">
-        <Advertise/>
+        <Advertise />
       </div>
 
       <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7">
-       
+        {posts && posts.length > 0 && (
+          <div className="flex flex-col gap-6">
+            <h2 className="text-2xl font-bold text-slate-700">Recent Posts</h2>
+
+            <div className="flex flex-wrap gap-4">
+              {posts.map((post) => (
+                <PostCard key={post._id} post={post} />
+              ))}
+            </div>
+
+            <Link
+              to={"/search"}
+              className="text-lg hover:underline text-center font-semibold"
+            >
+              View all news
+            </Link>
+          </div>
+        )}
       </div>
     </div>
   )
